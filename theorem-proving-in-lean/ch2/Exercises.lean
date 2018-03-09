27ccce9d098f2e6168cd7cd43cef9c498380e036
Mike Harris <MikeMKH@users.noreply.github.com>
2018-03-09 06:47:51 -0600

    Added in 2.2 - 2.4

--- theorem-proving-in-lean/ch2/Exercises.lean
+++ theorem-proving-in-lean/ch2/Exercises.lean
@@ -17,3 +17,44 @@ def Do_Twice : ((ℕ → ℕ) → (ℕ → ℕ)) → (ℕ → ℕ) → (ℕ → 
 #check Do_Twice do_twice
 #reduce Do_Twice do_twice
 #eval Do_Twice do_twice double 2
+
+-- 2
+def add_tuple : (ℕ × ℕ) → ℕ :=  λ x, x.1 + x.2
+#eval add_tuple (2, 3)
+
+def curry (α β γ : Type) (f : α × β → γ) : α → β → γ :=
+  λ x y, f (x, y)
+
+#check curry ℕ ℕ ℕ add_tuple
+#eval (curry ℕ ℕ ℕ add_tuple) 2 3
+
+def uncurry (α β γ : Type) (f : α → β → γ) : α × β → γ :=
+  λ (x : α ×  β), f x.1 x.2
+
+#check uncurry ℕ ℕ ℕ (curry ℕ ℕ ℕ add_tuple)
+#eval uncurry ℕ ℕ ℕ (curry ℕ ℕ ℕ add_tuple) (2, 3)
+
+-- 3
+universe u
+constant vec : Type u → ℕ → Type u
+
+constant vec_add :
+  Π (α : Type) (n : ℕ), vec α n → vec α n → vec α n
+ 
+constant vec_reserve :
+  Π (α : Type) (n : ℕ), vec α n → vec α n
+
+-- need to check
+
+-- 4  
+constant matrix : Type u → ℕ → ℕ → Type u
+
+constant matrix_add :
+  Π (α : Type) (n : ℕ) (m : ℕ),
+    matrix α n m → matrix α n m → matrix α n m
+    
+constant matrix_mul :
+  Π (α : Type) (n : ℕ) (m : ℕ) (p : ℕ),
+    matrix α n m → matrix α m p → matrix α n p
+    
+-- need to check 
