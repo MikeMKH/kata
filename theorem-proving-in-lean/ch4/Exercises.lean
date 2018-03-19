97971cd892f2050af41e1081070751259150279a
Mike Harris <MikeMKH@users.noreply.github.com>
2018-03-19 06:43:57 -0500

    Added 4.2

--- theorem-proving-in-lean/ch4/Exercises.lean
+++ theorem-proving-in-lean/ch4/Exercises.lean
@@ -25,3 +25,32 @@ example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
     from or.elim h
       (assume hp : ∀ x, p x, or.intro_left (q x) (hp x))
       (assume hq : ∀ x, q x, or.intro_right (p x) (hq x))
+      
+-- 2
+
+variable r : Prop
+
+example : α → ((∀ x : α, r) ↔ r) :=
+  assume a : α,
+  iff.intro
+   (assume h : α → r, h a)
+   (assume (r : r) (x : α), r)
+   
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r :=
+  have h₁ : (∀ x, p x ∨ r) → (∀ x, p x) ∨ r, from
+    assume hx : ∀ x, p x ∨ r, sorry,
+  have h₂ : (∀ x, p x) ∨ r → (∀ x, p x ∨ r), from
+    assume hx : (∀ x, p x) ∨ r,
+    take x : α, or.elim hx
+      (assume hp : ∀ x, p x, or.intro_left r (hp x))
+      or.inr,
+  iff.intro h₁ h₂
+  
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) :=
+  have h₁ : (∀ x, r → p x) → (r → ∀ x, p x), from
+    assume (hx : ∀ x, r → p x) r,
+    take x : α, hx x r,
+  have h₂ : (r → ∀ x, p x) → (∀ x, r → p x), from
+    assume hr : r → ∀ x, p x,
+    take x : α, assume r, hr r x,
+  iff.intro h₁ h₂
