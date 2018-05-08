08c3ccc75773d873dd5ffc20a8435720a7d3ad03
Mike Harris <MikeMKH@users.noreply.github.com>
2018-05-08 06:50:39 -0500

    Added Traverse

--- functional-programming-in-csharp/ch13/UnitTest1.cs
+++ functional-programming-in-csharp/ch13/UnitTest1.cs
@@ -3,7 +3,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using LaYumba.Functional;
 using static LaYumba.Functional.F;
+using Int = LaYumba.Functional.Int;
+using String = LaYumba.Functional.String;
 using Xunit;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ch13
 {
@@ -39,5 +43,62 @@ namespace ch13
         
         static Func<double, double, double> PickLowest
           = (x, y) => x < y ? x : y;
+          
+       [Theory]
+       [InlineData("1,2,3", new int[] {1, 2, 3})]
+       [InlineData("1, 2, 3", new int[] {1, 2, 3})]
+       [InlineData("one,2,three", new int[] {})]
+       [InlineData(",,", new int[] {})]
+       public void GivenStringMustParseToExpectedArray(string given, int[] expected)
+       {
+           Func<string, IEnumerable<int>> parse =
+             s => s
+                    .Split(',')
+                    .Map(String.Trim)
+                    .Traverse(Int.Parse)
+                    .GetOrElse(new int[] {});
+                    
+            Assert.Equal(expected, parse(given));
+       }
+          
+       [Theory]
+       [InlineData("1,2,3")]
+       [InlineData("1, 2, 3")]
+       [InlineData("one,2,three")]
+       [InlineData(",,")]
+       public void GivenStringMustParseToSameValueIfMonadicOrApplicativeTraversalIsUsed(string given)
+       {
+           Func<string, IEnumerable<int>> applicative =
+             s => s
+                    .Split(',')
+                    .Map(String.Trim)
+                    .TraverseA(Int.Parse)
+                    .GetOrElse(new int[] {});
+                    
+            Func<string, IEnumerable<int>> monadic =
+              s => s
+                     .Split(',')
+                     .Map(String.Trim)
+                     .TraverseM(Int.Parse)
+                     .GetOrElse(new int[] {});
+                    
+            Assert.Equal(applicative(given), monadic(given));
+       }
+       
+    //    [Fact]
+    //    public void ApplicativeTraversalIsAbleToGiveEachException()
+    //    {
+    //        var errors = "error 1,2,error 3"
+    //                       .Split(',')
+    //                       .Map(String.Trim)
+    //                       .TraverseA(Int.Parse)
+    //                       .Map(Enumerable.Sum)
+    //                       .Match(
+    //                           errs => string.Join(",", errs),
+    //                           sum => $"the sum is {sum}"
+    //                       );
+                              
+    //         Assert.Equal("error 1,error 3", errors);
+    //    }
     }
 }
