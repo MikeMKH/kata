43c6df5ce65047848a4c889019a871329551d26d
Mike Harris <MikeMKH@users.noreply.github.com>
2018-06-04 06:43:36 -0500

    Added 4.5 - 4.6

--- how-to-design-programs/ch4/Exercises.rkt
+++ how-to-design-programs/ch4/Exercises.rkt
@@ -40,3 +40,39 @@
 ; [3, 5) = 3, 4
 ; (3, 5) = 4
 
+; skipped 53
+
+(check-expect (not #false) #true)
+
+(require 2htdp/image)
+
+(define HEIGHT 300) ; distances in pixels 
+(define WIDTH  100)
+(define YDELTA 3)
+ 
+(define BACKG  (empty-scene WIDTH HEIGHT))
+(define ROCKET (rectangle 5 30 "solid" "red"))
+ 
+(define CENTER (/ (image-height ROCKET) 2))
+
+(define (place-rocket position)
+  (place-image ROCKET (- position CENTER) BACKG))
+
+; skipped 56 and 57
+
+(define (sales-tax p)
+  (cond
+    [(and (<= 0 p)
+          (< p 1000)) 0]
+    [(and (<= 1000 p)
+          (< p 10000)) (* 0.05 p)]
+    [(>= p 10000) (* 0.08 p)]))
+
+(check-expect (sales-tax 0) 0)
+(check-expect (sales-tax 999) 0)
+(check-expect (sales-tax 1000) (* 0.05 1000))
+(check-expect (sales-tax 9999) (* 0.05 9999))
+(check-expect (sales-tax 10000) (* 0.08 10000))
+(check-expect (sales-tax 10001) (* 0.08 10001))
+
+; I am not sure if BSL supports lambda :(
