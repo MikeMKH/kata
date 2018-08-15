41a6f9aaf5790daa533f9ad0d85749b57adcfffc
Mike Harris <MikeMKH@users.noreply.github.com>
2018-08-15 06:41:48 -0500

    Added 17.3

--- how-to-design-programs/ch17/Exercises.rkt
+++ how-to-design-programs/ch17/Exercises.rkt
@@ -34,3 +34,48 @@
  (
   ((lambda (x) (x x)) (lambda (x) x))
   "identity"))
+
+(check-expect (convert-euro '()) '())
+(check-expect (convert-euro (list 1 10 100 22.22)) (list 1.06 10.6 106.0 23.5532))
+
+(define (convert-euro usds)
+  (map (lambda (usd) (* usd 1.06)) usds))
+
+(check-expect (convertFC '()) '())
+(check-expect (convertFC (list -459.67 32 212)) (list -273.15 0 100))
+
+(define (convertFC Fs)
+  (map (lambda (F) (* (- F 32) 5/9)) Fs))
+
+(check-expect (translate '()) '())
+(check-expect (translate (list (make-posn 1 2) (make-posn 4 2))) (list '(1 2) '(4 2)))
+
+(define (translate posns)
+  (map (lambda (posn) (list (posn-x posn) (posn-y posn))) posns))
+
+; skipped 286 - 287
+
+(check-expect (build-list 3 (lambda (x) x)) '(0 1 2))
+(check-expect (build-list 3 add1) '(1 2 3))
+(check-expect (build-list 3 (lambda (x) (/ 1 (add1 x)))) '(1 1/2 1/3))
+(check-expect (build-list 3 (lambda (x) (* 2 x))) '(0 2 4))
+
+(check-expect (find-name "jack" '()) #false)
+(check-expect (find-name "jack" (list "jack")) #true)
+(check-expect (find-name "jack" (list "Jack")) #true)
+(check-expect (find-name "jack" (list "Moe" "Jack" "Jill")) #true)
+(check-expect (find-name "jack" (list "Moe" "Mo")) #false)
+(check-expect (find-name "j" (list "Moe" "Mo")) #false)
+(check-expect (find-name "j" (list "Jack" "Jill")) #true)
+
+(define (find-name name names)
+  (ormap (lambda (n) (string-contains-ci? name n)) names))
+
+(check-expect (start-name "j" (list "Moe" "Mo")) #false)
+(check-expect (start-name "j" (list "Jack" "Jill")) #true)
+(check-expect (start-name "J" (list "Jack" "Jill")) #true)
+
+(define (start-name name names)
+  (andmap (lambda (n) (string-ci=? name (string-ith n 0))) names))
+
+; todo 290 - 291
