a8a204a0e8fcb105483a416f00e45399495781b9
Mike Harris <MikeMKH@users.noreply.github.com>
2018-09-18 06:43:20 -0500

    Added 22.1

---
+++ how-to-design-programs/ch22/Exercises.rkt
@@ -0,0 +1,74 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname Exercises) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; skipped 363
+
+(define x364-1 '(transition ((from "seen-e") (to "seen-f"))))
+(define x364-2 '(ul (li (word) (word)) (li (word))))
+
+; skipped 365
+
+(define a0 '((initial "X")))
+ 
+(define e0 '(machine))
+(define e1 `(machine ,a0))
+(define e2 '(machine (action)))
+(define e3 '(machine () (action)))
+(define e4 `(machine ,a0 (action) (action)))
+
+(check-expect (xexpr-attr e0) '())
+(check-expect (xexpr-attr e1) '((initial "X")))
+(check-expect (xexpr-attr e2) '())
+(check-expect (xexpr-attr e3) '())
+(check-expect (xexpr-attr e4) '((initial "X")))
+
+; Figure 126
+(define (xexpr-attr xe)
+  (local ((define optional-loa+content (rest xe)))
+    (cond
+      [(empty? optional-loa+content) '()]
+      [else
+       (local ((define loa-or-x
+                 (first optional-loa+content)))
+         (if (list-of-attributes? loa-or-x)
+             loa-or-x
+             '()))])))
+
+(define (list-of-attributes? x)
+  (cond
+    [(empty? x) #true]
+    [else
+     (local ((define possible-attribute (first x)))
+       (cons? possible-attribute))]))
+
+(check-expect (xexpr-name e0) 'machine)
+(check-expect (xexpr-name e1) 'machine)
+(check-expect (xexpr-name e2) 'machine)
+(check-expect (xexpr-name e3) 'machine)
+(check-expect (xexpr-name e4) 'machine)
+
+(check-error (xexpr-name '()) "xexpr-name: invalid xexpr")
+
+(define (xexpr-name xe)
+  (cond
+    [(empty? xe) (error "xexpr-name: invalid xexpr")]
+    [else (first xe)]))
+
+(check-expect (xexpr-content e0) '())
+(check-expect (xexpr-content e1) '())
+(check-expect (xexpr-content e2) '((action)))
+(check-expect (xexpr-content e3) '((action)))
+(check-expect (xexpr-content e4) '((action) (action)))
+
+(define (xexpr-content xe)
+  (local ((define optional-loa+content (rest xe)))
+    (cond
+      [(empty? optional-loa+content) '()]
+      [else
+       (local ((define loa-or-x
+                 (first optional-loa+content)))
+         (if (not (list-of-attributes? loa-or-x))
+             optional-loa+content
+             (rest optional-loa+content)))])))
+
+; skipped 367 - 369
